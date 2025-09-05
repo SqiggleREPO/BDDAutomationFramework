e6a2e3d1e9979bfec7ef96907a6f8d911f1de6c6
SqiggleREPO <marccondy@gmail.com>
2025-09-05 17:43:25 +0100

    add methods

--- AppSpecFlow/features/Project1/test-0001.feature
+++ AppSpecFlow/features/Project1/test-0001.feature
@@ -4,9 +4,16 @@ Feature: test-0001 Login Functionality
 @test-0001
 Scenario Outline: test-0001-testing login with valid credentials
     Given I am testing "Project1"
-    ##And I have navigated to the application
-    ##When I enter "<username>" into "username" field
-    ##And I enter "<password>" into "password" field
-    ##And I click on "loginbutton" element
+    And I have navigated to the application
+    Then page "Swag Labs" is displayed
+
+    When I enter "standard_user" in textbox "Username"
+    Then textbox "Username" contains "standard_user"
+
+    When I enter "secret_sauce" in textbox "Password"
+    Then textbox "Password" contains "secret_sauce"
+
+    When I click button "Login"
+    #Then page "Products" is displayed
     ##Then I should see "dashboard" element
     ##And I should see text "Welcome"
--- AppTargets/Forms/Project/FirstPage.cs
+++ AppTargets/Forms/Project/FirstPage.cs
@@ -1,19 +1,17 @@
 using OpenQA.Selenium;
 using Core;
 
-namespace AppTargets.Forms.Project
+namespace AppTargets.Forms
 {
     public class FirstPage : FormBase
     {
         public FirstPage() : base(By.Id("First"), "First page")
         {
-            /// Add Elements - Element Identifiers are LOWER case (apart from ID)
-            ///
-            Elements.Add("ID", By.XPath("//mat-card-title[contains(text(),'Maintain Non-Primary Charge Types')]"));
-            Elements.Add("username", By.Id("username"));
-            Elements.Add("password", By.Id("password"));
-            Elements.Add("loginbutton", By.XPath("//button[contains(text(),'Login')]"));
-            Elements.Add("errormessage", By.ClassName("error-message"));
+
+            Elements.Add("username", By.XPath("/html/body/div/div/div[2]/div[1]/div/div/form/div[1]/input"));
+            Elements.Add("password", By.XPath("//input[@type='password']"));
+            Elements.Add("swaglabs", By.XPath("//div[@class='Swag Labs']"));
+            Elements.Add("login", By.XPath("//input[@id='login-button']"));
             Elements.Add("dashboard", By.Id("dashboard-container"));
             
             // Also populate XPath dictionary if needed
