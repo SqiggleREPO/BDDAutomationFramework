3bc5e133071c51784e0df6f113e3a43dcefe404e
SqiggleREPO <marccondy@gmail.com>
2025-09-05 12:23:40 +0100

    update

--- Generic/obj/Debug/net8.0/Generic.AssemblyInfo.cs
+++ Generic/obj/Debug/net8.0/Generic.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Generic")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+90d2cd954154a5e7316aaa0bfcb615ed9e2a24e9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+098af881471809fa3c7f15e8c757d216ebc4dfc6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Generic")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Generic")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
